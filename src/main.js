e9fb538ce6d409d0342d65108d1b2f5ac551a505
chrisfan <yukun0620@gmail.com>
2024-11-25 20:52:06 +0800

    getFile

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ export default async ({ req, res, log, error }) => {
     .setProject('674459e4001ac3695027')
     .setKey(req.headers['x-appwrite-key'] ?? '');
   const storage = new Storage(client);
-  const result = await storage.getFileDownload(
+  const result = await storage.getFile(
     '67446e2d0030235b1ba4', // bucketId
-    '67446e590027a7ed5c65', // queries (optional)
-  );
-  console.log(result);
+    '67446e590027a7ed5c65' // fileId
+);
+  context.log(result);
 
   // The req object contains the request data
   if (req.path === "/ping") {
