ad402fac912e75e6afb601c197dccc669ddc4543
chrisfan <yukun0620@gmail.com>
2024-11-25 20:56:37 +0800

    get file

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ export default async ({ req, res, log, error }) => {
     .setProject('674459e4001ac3695027')
     .setKey(req.headers['x-appwrite-key'] ?? '');
   const storage = new Storage(client);
-  const result = await storage.getFile(
+  const result = await storage.getFileDownload(
     '67446e2d0030235b1ba4', // bucketId
     '67446e590027a7ed5c65' // fileId
 );
