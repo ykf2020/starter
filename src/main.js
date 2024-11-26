929109d82cbdc0dcdf553490c16535ec9059d34f
chrisfan <yukun0620@gmail.com>
2024-11-26 09:56:37 +0800

    remove storage

--- src/main.js
+++ src/main.js
@@ -7,12 +7,6 @@ export default async ({ req, res, log, error }) => {
     .setEndpoint('https://67445ab24213a6ce118e.appwrite.global/')
     .setProject('674459e4001ac3695027')
     .setKey(req.headers['x-appwrite-key'] ?? '');
-  const storage = new Storage(client);
-  const result = await storage.getFileDownload(
-    '67446e2d0030235b1ba4', // bucketId
-    '67446e590027a7ed5c65' // fileId
-);
-  context.log(result);
 
   // The req object contains the request data
   if (req.path === "/ping") {
