0c1abb94b8711b422061f5b408c6299243b1dcf5
chrisfan <yukun0620@gmail.com>
2024-11-26 10:53:22 +0800

    remove assets in dir

--- src/assets/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Page loaded!');
-    
-    document.querySelectorAll('a').forEach(link => {
-      link.addEventListener('click', (e) => {
-        console.log('Clicked:', e.target.href);
-      });
-    });
-  });
