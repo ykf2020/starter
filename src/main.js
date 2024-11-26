72134b6ea81e36b416c6f6004356a60ebb8ce0be
chrisfan <yukun0620@gmail.com>
2024-11-26 10:35:16 +0800

    try get

--- src/main.js
+++ src/main.js
@@ -1,24 +1,37 @@
 import { Client, Storage } from 'node-appwrite';
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
+  const projectId = '674459e4001ac3695027';
+  const bucketId = '67446e2d0030235b1ba4';
   // You can use the Appwrite SDK to interact with other services
   // For this example, we're using the Users service
   const client = new Client()
-    .setEndpoint('https://67445ab24213a6ce118e.appwrite.global/')
-    .setProject('674459e4001ac3695027')
-    .setKey(req.headers['x-appwrite-key'] ?? '');
+    .setEndpoint('https://cloud.appwrite.io/v1')
+    .setProject(projectId)
 
   const storage = new Storage(client);
-//   const htmlFile = await storage.getFile(
-//     '67446e2d0030235b1ba4',
-//     '67446e590027a7ed5c65'
-// );
+  const getHtmlResult = await storage.listFiles(
+    bucketId,
+    [],
+    '.html'
+);
 
-  // The req object contains the request data
+
   if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
     return res.text("Pong");
   }
-  return res.send(htmlFile, 200, {'content-type': 'text/html'});
+
+  
+  try {
+
+    const htmlFileId = getHtmlResult.files[0]['$id'];
+    const htmlFileLink = `https://cloud.appwrite.io/v1/storage/buckets/${bucketId}/files/${htmlFileId}/view?project=${projectId}`
+    const response = await fetch(htmlFileLink);
+    const htmlContent = await response.text();
+    
+    return res.send(htmlContent, 200, {'content-type': 'text/html'});
+  } catch (err) {
+    log(err);
+    return res.json({ error: 'Failed to fetch HTML content' }, 500);
+  }
 };
