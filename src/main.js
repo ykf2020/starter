d95708ec72a694b1874606f0f2dbe34d27f732c1
chrisfan <yukun0620@gmail.com>
2024-11-26 10:09:38 +0800

    get html from storage

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { Client } from 'node-appwrite';
+import { Client, Storage } from 'node-appwrite';
 // This Appwrite function will be executed every time your function is triggered
 export default async ({ req, res, log, error }) => {
   // You can use the Appwrite SDK to interact with other services
@@ -8,20 +8,17 @@ export default async ({ req, res, log, error }) => {
     .setProject('674459e4001ac3695027')
     .setKey(req.headers['x-appwrite-key'] ?? '');
 
+  const storage = new Storage(client);
+  const htmlFile = await storage.getFile(
+    '67446e2d0030235b1ba4', // bucketId
+    '67446e590027a7ed5c65' // fileId
+);
+
   // The req object contains the request data
   if (req.path === "/ping") {
     // Use res object to respond with text(), json(), or binary()
     // Don't forget to return a response!
     return res.text("Pong");
   }
-  const htmlLink = 'https://cloud.appwrite.io/v1/storage/buckets/67446e2d0030235b1ba4/files/67446e590027a7ed5c65/view?project=674459e4001ac3695027&project=674459e4001ac3695027&mode=admin'
-  try {
-    const response = await fetch(htmlLink);
-    const htmlContent = await response.text();
-    
-    return res.send(htmlContent, 200, {'content-type': 'text/html'});
-  } catch (err) {
-    log(err);
-    return res.json({ error: 'Failed to fetch HTML content' }, 500);
-  }
+  return res.send(htmlFile, 200, {'content-type': 'text/html'});
 };
